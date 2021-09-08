4b1e2cf04f9d6bf1f17f70c9958107632ef82cd0
schastlivy@icloud.com <m.schastlivyy@betaonline.ru>
2021-09-08 14:15:57 +0300

    Homework6

--- src/test/java/BaseApiTest.java
+++ src/test/java/BaseApiTest.java
@@ -1,4 +1,11 @@
+import org.junit.jupiter.api.Assertions;
+
 import java.io.IOException;
+import java.lang.reflect.Field;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 abstract class BaseApiTest {
 
