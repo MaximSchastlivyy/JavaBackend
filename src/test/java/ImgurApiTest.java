6f5e01da784e2f6737e7e5adc5281681e2623854
schastlivy@icloud.com <m.schastlivyy@betaonline.ru>
2021-09-13 18:56:50 +0300

    1

--- src/test/java/ImgurApiTest.java
+++ src/test/java/ImgurApiTest.java
@@ -2,38 +2,39 @@ import java.io.File;
 import java.io.IOException;
 
 import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.http.ContentType;
 import io.restassured.http.Header;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import org.hamcrest.Matchers;
+import org.junit.Test;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 
-import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
 
 public class ImgurApiTest extends BaseApiTest {
 
-    private String currentDeleteHash;
-
     public ImgurApiTest() throws IOException {
     }
-
     @BeforeEach
-    void setUp() {
+    public void setUp() {
         RestAssured.baseURI = getBaseUri();
     }
 
-
     @AfterEach
-    void tearDown() {
-
+    public void tearDown() {
     }
 
     @Test
     @DisplayName("Получение информации об аккаунте")
-    void testGetAccountBase() {
+    public void testGetAccountBase() {
 
         given()
                 .contentType(ContentType.JSON)
@@ -41,34 +42,11 @@ public class ImgurApiTest extends BaseApiTest {
                 .auth()
                 .oauth2(getToken())
                 .expect()
-                .body("data.url", is("levinmk23"))
+                .body("data.url", is("SchastlivyMax"))
                 .log()
                 .all()
                 .statusCode(200)
                 .when()
                 .get("3/account/{username}", getUserName());
     }
-
-    @Test
-    @DisplayName("Тест загрузки картинки")
-    void testImageUpload() throws Exception {
-
-        currentDeleteHash = given()
-                .auth()
-                .oauth2(getToken())
-                .when()
-                .header(new Header("content-type", "multipart/form-data"))
-                .multiPart("image", new File( "./src/main/resources/res.jpg"))
-                .expect()
-                .statusCode(200)
-                .body("data.id", is(notNullValue()))
-                .body("data.deletehash", is(notNullValue()))
-                .log()
-                .all()
-                .when()
-                .post("3/upload")
-                .jsonPath()
-                .getString("data.deletehash");
-    }
-
 }
